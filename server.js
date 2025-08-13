5230b8187abbd46a4dd608cfe259e8fe41a5704e
Yashwanth45-jpg <myashwanth45@gmail.com>
2025-08-13 13:10:30 +0530

    just update

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL, // Your frontend URL
     methods: ["GET", "POST", "PUT"],
-    credentials: true 
+    credentials: true,
   }
 });
 
